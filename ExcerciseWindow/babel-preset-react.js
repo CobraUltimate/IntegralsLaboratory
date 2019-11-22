3c03e726116c1ebdd3b73122d7e03a8c3e9f9cf9
CobraUltimate <carrocobra2@hotmail.com>
2019-11-21 22:50:47 -0600

    commit

---
+++ ExcerciseWindow/babel-preset-react.js
@@ -0,0 +1,17 @@
+module.exports = () => ({
+  "presets": [
+      "@babel/preset-env",
+      "@babel/preset-react"
+  ],
+  "plugins": [
+      [
+        "@babel/plugin-proposal-class-properties",
+        {
+          "loose": true
+        }
+      ],
+      [
+        "transform-remove-strict-mode"
+      ]
+  ]
+})
